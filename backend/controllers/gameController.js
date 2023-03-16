c3b1e3d1d842e9972e6ce6df69c85793eb6437c9
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-03-16 23:40:53 +0100

    Update gameController.js

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -26,7 +26,7 @@ exports.getWordPackCWG = async (req, res, next) => {
     return res.status(200).json({ wordPack });
   } catch (error) {
     console.error('GET WORD PACK CWG ERROR: ', error);
-    return res.status(503).json({ message: 'Lỗi dịch vụ, thử lại sau' });
+    return res.status(503).json({ message: 'Service error, try again later' });
   }
 };
 
@@ -47,7 +47,7 @@ exports.getWordPackWMG = async (req, res, next) => {
     return res.status(200).json({ wordPack: [] });
   } catch (error) {
     console.error('GET WORD PACK WMG ERROR: ', error);
-    return res.status(503).json({ message: 'Lỗi dịch vụ, thử lại sau' });
+    return res.status(503).json({ message: 'Service error, try again later' });
   }
 };
 
@@ -68,6 +68,6 @@ exports.getWordPackFS = async (req, res, next) => {
     return res.status(200).json({ wordPack });
   } catch (error) {
     console.error('GET WORD PACK FAST GAME ERROR: ', error);
-    return res.status(500).json({ message: 'Lỗi dịch vụ, thử lại sau' });
+    return res.status(500).json({ message: 'Service error, try again later' });
   }
 };
