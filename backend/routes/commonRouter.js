5e25aba65f953e8b0fd66f6c28a7ba50d504ace8
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:29:23 +0200

    Update commonRouter.js

--- backend/routes/commonRouter.js
+++ backend/routes/commonRouter.js
@@ -3,4 +3,3 @@ const commonController = require('../controllers/commonController');
 commonRouter.get('/word-pack/total', commonController.getTotalWordPack);
 commonRouter.get('/word-topic/total', commonController.getTotalWordPackTopic);
 module.exports = commonRouter;
- 
