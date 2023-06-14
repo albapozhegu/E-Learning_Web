97972cc99358222fcd35752f2ea0438fd39f71b3
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:28:34 +0200

    Update faqRouter.js

--- backend/routes/Extras/faqRouter.js
+++ backend/routes/Extras/faqRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controller = require('../../controllers/Extras/faqController');
 const { authentication, authRole } = require('../../middlewares/authenticationMiddleware');
-
+  
 router.get('/get-all-faq',authentication, controller.getFAQ);
 //router.get('/get-faq-by-id',authentication, null);
 router.put('/put-faq/:id',authentication, controller.updateFAQ)
