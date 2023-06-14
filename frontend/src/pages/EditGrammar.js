c1341a6d384a3e8ec31cac36155189a9d40142b9
Blendi Gashi <104309667+Blendig@users.noreply.github.com>
2023-06-14 23:57:02 +0200

    Update EditGrammar.js

--- frontend/src/pages/EditGrammar.js
+++ frontend/src/pages/EditGrammar.js
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyle = makeStyles(() => ({
   root: {
     margin: "3.2rem 0",
-  },
+  }, 
 }));
 function EditGrammarPage() {
   const classes = useStyle();
