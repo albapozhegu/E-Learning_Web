04470496b31d217f9fcb995731716ffe588a1524
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:29:52 +0200

    Update grammarRouter.js

--- backend/routes/grammarRouter.js
+++ backend/routes/grammarRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const grammarController = require('../controllers/grammarController');
 const { authentication, authRole } = require('../middlewares/authenticationMiddleware');
-
+  
 router.get('/get-all-grammar', authentication, grammarController.getAllGrammars);
 router.get('/get-grammar-by-id/:id',authentication, grammarController.getById);
 router.get('/get-grammar/:id',authentication, grammarController.getGrammar);
@@ -11,6 +11,5 @@ router.post('/post-grammar',authentication, grammarController.postGrammar);
 router.put('/put-grammar/:id',authentication, grammarController.putGrammar);
 router.patch('/delete-grammar/:id',authentication, grammarController.deleteById);
 router.get('/search-grammar',authentication, grammarController.getSearchGrammar);
-
 
 module.exports = router;
