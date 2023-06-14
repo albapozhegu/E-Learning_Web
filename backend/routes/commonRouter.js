1ec47e1d4238b4e764e5ce9d97bf733e6aa36f49
Blendi Gashi <104309667+Blendig@users.noreply.github.com>
2023-06-14 23:59:41 +0200

    Update commonRouter.js

--- backend/routes/commonRouter.js
+++ backend/routes/commonRouter.js
@@ -3,3 +3,4 @@ const commonController = require('../controllers/commonController');
 commonRouter.get('/word-pack/total', commonController.getTotalWordPack);
 commonRouter.get('/word-topic/total', commonController.getTotalWordPackTopic);
 module.exports = commonRouter;
+ 
