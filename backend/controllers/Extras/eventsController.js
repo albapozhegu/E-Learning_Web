d3c7653ff3693bb11dc5942b5c63b5b9eb3f389d
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-14 23:46:59 +0200

    Update eventsController.js

--- backend/controllers/Extras/eventsController.js
+++ backend/controllers/Extras/eventsController.js
@@ -41,7 +41,7 @@ exports.updateEvent = async(req, res) => {
     try {
         const update = await EventsModel.findByIdAndUpdate(_id, { date, duration, title, host, status })
         if (update) {
-            return res.status(200).json({ message: 'Success' })
+            return res.status(200).json({ message: 'success' })
         }
     } catch (error) {
         return res.status(500).json({ message: 'Operation failed' })
@@ -59,6 +59,6 @@ exports.deleteEvent = async(req, res) => {
         console.error("DELETE ERROR: ", error);
         return res
             .status(503)
-            .json({ message: "Error, can not delete this question" });
+            .json({ message: "Eror, can not delete this question" });
     }
 }
