19108f3c4ff1783d234aebccea4ad6b8ccb82676
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:27:32 +0200

    Update booksRouter.js

--- backend/routes/Extras/booksRouter.js
+++ backend/routes/Extras/booksRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controller = require('../../controllers/Extras/booksController');
 const { authentication, authRole } = require('../../middlewares/authenticationMiddleware');
-
+  
 router.get('/get-all-book',authentication, controller.getBooks);
 //router.get('/get-book-by-id',authentication, null);
 router.put('/put-book/:id',authentication, controller.updateBooks)
