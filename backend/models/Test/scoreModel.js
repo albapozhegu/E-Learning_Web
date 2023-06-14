bb858f224ae47101a222d85ec95641c5652e3a11
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:20:05 +0200

    Update scoreModel.js

--- backend/models/Test/scoreModel.js
+++ backend/models/Test/scoreModel.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-//const QuestionTest = require('./test/questionTestModel.model');
-
 const ScoreModel = new Schema({
+  
   Sentences: {
     type: Number
   },
-
   Score: {
     type: Number
   }
