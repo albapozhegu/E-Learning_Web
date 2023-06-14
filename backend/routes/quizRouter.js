6dea18125300c34c01a5f5fe0a05e7deceacee77
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:32:36 +0200

    Update quizRouter.js

--- backend/routes/quizRouter.js
+++ backend/routes/quizRouter.js
@@ -2,7 +2,7 @@ const quizApi = require('express').Router();
 const { Router } = require('express');
 const quizController = require('../controllers/quizController');
 const { authentication, authRole } = require('../middlewares/authenticationMiddleware');
-
+  
 quizApi.get('/get-all-quiz',authentication, quizController.getAllQuizzes);
 quizApi.get('/get-quiz-by-id/:id',authentication, quizController.getById);
 quizApi.get('/get-quiz-by-listen/:id',authentication, quizController.getByListeningId);
