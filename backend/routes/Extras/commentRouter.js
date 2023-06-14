08170308f5af7d29d0b362a0d9e8cb43b90042a5
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:27:38 +0200

    Update commentRouter.js

--- backend/routes/Extras/commentRouter.js
+++ backend/routes/Extras/commentRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controller = require('../../controllers/Extras/commentsController');
 const { authentication, authRole } = require('../../middlewares/authenticationMiddleware');
-
+  
 router.get('/get-all-comment',authentication, controller.getComment);
 router.get('/get-comment-by-id',authentication,controller.getCommentByForumId);
 router.put('/put-comment/:id',authentication, controller.updateComment)
