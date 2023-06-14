1a30f5dac98072b985fe1fd24f947d0c2f6cfdd6
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-14 23:48:55 +0200

    Update storiesController.js

--- backend/controllers/Extras/storiesController.js
+++ backend/controllers/Extras/storiesController.js
@@ -1,55 +1,54 @@
 const StoriesModel = require('../../models/Extras/storiesModel')
-exports.addStories = async(req, res) => {
+exports.addStories = async (req, res) => {
     try {
-        const { createdBy, content } = req.body;
-        const forumInfo = {
-            createdBy,
-            content
-
+        const { title,content, status } = req.body;
+        const storiesInfo = {
+           title, content, status
         }
-        const newStories = await StoriesModel.create(forumInfo)
+        const newStories = await StoriesModel.create(storiesInfo)
         if (newStories) {
-            return res.status(200).json({ message: 'Success' })
+            return res.status(200).json({ newStories })
         }
     } catch (error) {
-        return res.status(500).json({ message: 'Could not create blog' })
+        return res.status(500).json({ message: 'Operation failed' })
 
     }
 }
-exports.getStories = async(req, res) => {
+exports.getStories = async (req, res) => {
     try {
-        let stories = await StoriesModel.find({})
-        if (stories) {
-            return res.status(200).json({ stories })
+        let storiess = await StoriesModel.find({})
+        if (storiess) {
+            return res.status(200).json({ data: storiess })
         }
+        return null
     } catch (error) {
         return res.status(500).json({ message: 'Operation failed' })
     }
 }
-exports.updateStories = async(req, res) => {
-    const id = { _id: req.params.id };
-    const { createdBy, content } = req.body;
+exports.updateStories = async (req, res) => {
+    const id = req.params.id;
+    const { content } = req.body;
     try {
-        const update = await StoriesModel.findByIdAndUpdate(id, { createdBy, content })
+        const update = await StoriesModel.findByIdAndUpdate(id, { title,content: content })
         if (update) {
-            return res.status(200).json({ message: 'Success' })
+            return res.status(200).json({ message: 'success' })
         }
     } catch (error) {
         return res.status(500).json({ message: 'Operation failed' })
 
     }
 }
-exports.deleteStories = async(req, res) => {
+exports.deleteStories = async (req, res) => {
     try {
-        const id = { _id: req.params.id };
-        const isDelete = await StoriesModel.findByIdAndDelete(id);
+        const id = req.params.id;
+        const isDelete = await StoriesModel.findByIdAndDelete({ _id: req.params.id });
         if (isDelete) {
-            return res.status(200).json({ message: "Deleted successfully." });
+            return res.status(200).json({ message: "Delete successfully." });
         }
     } catch (error) {
         console.error("DELETE ERROR: ", error);
         return res
             .status(503)
-            .json({ message: "Error, can not delete this question" });
+            .json({ message: "Eror, can not delete this question" });
     }
 }
