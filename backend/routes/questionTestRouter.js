13aeabc5e82b420f5f762200a7f0820f8fe60e5c
Blendi Gashi <104309667+Blendig@users.noreply.github.com>
2023-06-14 23:59:59 +0200

    Update questionTestRouter.js

--- backend/routes/questionTestRouter.js
+++ backend/routes/questionTestRouter.js
@@ -3,7 +3,7 @@ const questionTestController = require('../controllers/questionTestController');
 const { authentication, authRole } = require('../middlewares/authenticationMiddleware');
 
 //router.get('/get-all-questionTest', authentication, questionTestController.getByTestId);
-router.get('/get-questionTest-by-id/:id',authentication, questionTestController.getById);
+router.get('/get-questionTest-by-id/:id',authentication, questionTestController.getById); 
 router.get('/get-questionTest-by-file/:id',authentication, questionTestController.getByFileTestId);
 router.post('/post-questionTest/:id', authentication, questionTestController.postQuestion);
 router.post('/post-many-questionTest/:id', authentication, questionTestController.postManyQuestion);
