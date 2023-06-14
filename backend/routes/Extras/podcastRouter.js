2f72025cfc3d59f2fc00ac76b6739c99049dd6d0
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:28:53 +0200

    Update podcastRouter.js

--- backend/routes/Extras/podcastRouter.js
+++ backend/routes/Extras/podcastRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controller = require('../../controllers/Extras/podcastController');
 const { authentication, authRole } = require('../../middlewares/authenticationMiddleware');
-
+  
 router.get('/get-all-podcast',authentication, controller.getPodcast);
 //router.get('/get-podcast-by-id',authentication, null);
 router.put('/put-podcast/:id',authentication, controller.updatePodcast)
