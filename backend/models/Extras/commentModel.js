198bd520125d61b6783e69a509ca1fcf35e41462
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:11:22 +0200

    Update commentModel.js

--- backend/models/Extras/commentModel.js
+++ backend/models/Extras/commentModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const CommentModel = new Schema({
+    
     createdBy:{
         type:Schema.Types.ObjectId,
         required:true,
