001801c5ab197330593e9887bbee573bc6f6b437
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:27:03 +0200

    Update announcementRouter.js

--- backend/routes/Extras/announcementRouter.js
+++ backend/routes/Extras/announcementRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controller = require('../../controllers/Extras/announcementsController');
 const { authentication, authRole } = require('../../middlewares/authenticationMiddleware');
-
+  
 router.get('/get-all-announcement',authentication, controller.getAnnouncement);
 router.put('/put-announcement/:id',authentication, controller.updateAnnouncement)
 router.post('/post-announcement',authentication, controller.addAnnouncement)
