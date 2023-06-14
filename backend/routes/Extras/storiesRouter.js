a9ccfd3ce2b8aeb9484c740a2340b2e5e59ebc53
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:29:03 +0200

    Update storiesRouter.js

--- backend/routes/Extras/storiesRouter.js
+++ backend/routes/Extras/storiesRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controller = require('../../controllers/Extras/storiesController');
 const { authentication, authRole } = require('../../middlewares/authenticationMiddleware');
-
+  
 router.get('/get-all-stories',authentication, controller.getStories);
 //router.get('/get-stories-by-id',authentication, null);
 router.put('/put-stories/:id',authentication, controller.updateStories)
