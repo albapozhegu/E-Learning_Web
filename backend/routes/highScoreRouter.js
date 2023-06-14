c9a8dfd359ffb65e370c9213171287ea956884a5
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-15 00:30:04 +0200

    Update highScoreRouter.js

--- backend/routes/highScoreRouter.js
+++ backend/routes/highScoreRouter.js
@@ -1,9 +1,8 @@
 const highScoreApi = require('express').Router();
 const highScoreController = require('../controllers/highScoreController');
 const { authentication } = require("../middlewares/authenticationMiddleware");
-
+  
 highScoreApi.put('/update', authentication, highScoreController.putUpdateHighScore);
-
 highScoreApi.get('/leaderboard',authentication, highScoreController.getLeaderboard);
 highScoreApi.post('/post', authentication, highScoreController.postScore);
 
