8041b9201f905ba482449d8161f6402f8b87b972
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-16 22:17:51 +0200

    Update EditBook.js

--- frontend/src/components/Extras/BooksAdmin/EditBook.js
+++ frontend/src/components/Extras/BooksAdmin/EditBook.js
@@ -1,20 +1,24 @@
-import React,{useState} from 'react';
-import { Box, Grid, Typography,Button } from '@material-ui/core';
+import React, { useState,useEffect } from 'react';
+import { Box, Grid, Typography, Button } from '@material-ui/core';
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { makeStyles } from "@material-ui/core/styles";
 import useTitle from "hooks/useTitle";
-import InputCustom from "components/UI/InputCustom"; 
+import InputCustom from "components/UI/InputCustom";
 import * as yup from "yup";
 import SaveIcon from "@material-ui/icons/Save";
-
+import { useDispatch } from "react-redux";
+import { setMessage } from "redux/actions/messageAction";
+import { convertImageToBase64 } from "helper";
+import {useHistory,useParams} from 'react-router-dom'
 import LoopIcon from "@material-ui/icons/Loop";
+import bookApi from 'apis/Extras/booksApi';
 
 //-------------------------------------------------------------------------------------------------------------------
 const useStyle = makeStyles(() => ({
   wrap: {
     minHeight: "calc(100vh - 7.2rem)",
-    width:500
+    width: 'auto'
   },
 
   root: {
@@ -23,7 +27,7 @@ const useStyle = makeStyles(() => ({
     borderRadius: "var(--border-radius)",
     textAlign: "center",
     boxShadow: "var(--box-shadow)",
-    width:450
+    width: '80%'
   },
 
   avtWrap: {
@@ -31,8 +35,10 @@ const useStyle = makeStyles(() => ({
     height: "15rem",
     position: "relative",
   },
-
   avt: {
+
+  },
+  avti: {
     borderRadius: "50%",
     border: "2px solid var(--primary-color)",
   },
@@ -124,78 +130,268 @@ const useStyle = makeStyles(() => ({
   },
 }));
 const schema = yup.object().shape({
-  title: yup.string().required('Title is required'),
-  content: yup.string().required('Content is required')
+  title: yup.string().required('Book title is required'),
+  description: yup.string().required('Description is required'),
+  price: yup.number().required('Price is required'),
+  authour: yup.string().required('Authour name is required').min(3),
+
+
 });
 export default function EditBook() {
-  useTitle("Add Announcement");
+  useTitle("Add Book");
+
+  const {id} = useParams();
+
+  const defaultImg = `${process.env.REACT_APP_DEFAULT_BOOK_COVER}`
+
+
+  const [image, setImage] = useState(defaultImg);
+
+  const [cover, setCover]=useState(null);
   const [submitting, setSubmitting] = useState(false);
+  const dispatch = useDispatch();
+
+  const history=useHistory()
 
   const classes = useStyle();
 
+ const [book, setBook] = useState(null);
 
-  const handleAddUser = () => {
+    useEffect(() => {
 
+        (
+            async function () {
+                const c = await bookApi.getbookbyid(id);
+                if (c.status === 200) {
+                  const _book = c.data.book;
+                  if(_book.cover!==null){
+                    setImage(_book.cover)
+                  }
+                   
+                    setBook(_book)
+                }
+            }
+
+
+        )();
+    }, [])
+     console.log(book)
+  const handleBookUpdate = async(vals) => {
+    console.log(vals)
+    const { 
+      title,
+      price,
+      description,
+      authour } = vals;
+
+    const fd = new FormData();
+    fd.append('title', title)
+    fd.append('price', price)
+    fd.append('description', description)
+    fd.append('authour', authour)
+    
+   
+
+      try {
+        const req = await bookApi.updatebook(id,fd)
+        if(req.status===200){
+          console.log(req.data)
+          dispatch(setMessage(req.data.message||"Operation successful", "success"));
+          history.push('/admin/books')
+        }
+      } catch (error) {
+        const r = error&&error?.response
+        console.log(r)
+         dispatch(setMessage(r.data.message||"Something went wrong", "error"));
+      }
   }
 
-  const handleChange = () => {
-
-  }
   const {
-    createAnnouncement,
+    register,
+    handleChange,
     handleSubmit,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
+
+
+
+
+  const handleChangePicture = (e) => {
+    e.preventDefault();
+    try {
+      const file = e.target.files[0];
+      if (!file) {
+        dispatch(setMessage("No files were uploaded", "error"));
+      }
+      if (file.size / 1024 ** 2 > 2) {
+        dispatch(setMessage("Size too large", "error"));
+      }
+      convertImageToBase64(file).then((res) => {
+        setImage(res);
+      });
+      setCover(file)
+    } catch (err) {
+      throw err;
+    }
+  };
+
+const updateCover=async()=>{
+  try{
+    if(cover===null){
+        dispatch(setMessage("Click blue button to upload a new cover image", "error"));
+      //alert('Click blue button to upload a new cover')
+    }else{
+       const fd = new FormData();
+    fd.append('cover', cover)
+    const upt = await bookApi.updatecover(id,fd)
+      if(upt.status===200){
+         dispatch(setMessage(upt.data.message||"Operation successful", "success"));
+      }
+    }
+     }catch(error){
+ const r = error&&error?.response
+        
+         dispatch(setMessage(r.data.message||"Something went wrong", "error"));
+      
+  }
+}
+  
   return (
     <>
       <div className={`${classes.wrap} container flex-center`}>
         <div className={classes.root}>
-          <form autoComplete="off" onSubmit={handleSubmit(handleAddUser)}>
-            
+         {book!==null&& <form autoComplete="off" onSubmit={handleSubmit(handleBookUpdate)}>
+
+
             <Grid container direction='column' spacing={3}>
-            <Grid item>
-              <Typography>Add Announcement</Typography>
-            </Grid>
+              <Grid item>
+                <Typography>Edit Book</Typography>
+              </Grid>
+              <Grid item container alignContent="center">
+                <div className={classes.avtWrap}>
+                  <img
+                    src={image ? image : defaultImg}
+                    alt=""
+                    className={`${classes.avt} w-100 h-100`}
+                  />
+                  <div className={`${classes.cameraIconWrap} flex-center`}>
+                    <input
+                      type="file"
+                      className={classes.fileInput}
+                      onChange={handleChangePicture}
+                      accept="image/*"
+                    />
+                  </div>
+                </div>
+               
+
+              </Grid> 
+              <Grid>
+                   <Button onClick={updateCover} variant='outlined'>Update image</Button>
+              </Grid>
+              <br/>
+<div className='english-break'></div>
+<br/>
               <Grid item>
                 <InputCustom
                   label="Title"
                   size="small"
-                  placeholder="Enter Title"
+                  inputProps={{
+                     defaultValue:`${book!==null&&book.title||''}`,
+                    name: "title",
+                    ...register("title"),
+
+                  }}
+                  placeholder="Enter Book Title"
                   error={Boolean(errors.title)}
                   onChange={handleChange}
+                 
                   fullWidth
+
                 />
+                {errors.title && (
+                  <p className="text-error">{errors.title?.message}</p>
+                )}
+              </Grid>
+              <Grid item container spacing={2}>
+                <Grid item xs={6}>
+                  <InputCustom
+                    label="Authour"
+                    size="small"
+                    inputProps={{
+                       defaultValue:`${book!==null&&book.authour||''}`,
+                      name: "authour",
+                      ...register("authour"),
+                    }}
+                    placeholder="Enter Authour Name"
+                    error={Boolean(errors.authour)}
+                    onChange={handleChange}
+                    fullWidth
+                  />
+                  {errors.authour && (
+                    <p className="text-error">{errors.authour?.message}</p>
+                  )}
+                </Grid>
+                <Grid item xs={6}>
+                  <InputCustom
+                    label="Price"
+                    size="small"
+                    type='number'
+                    inputProps={{
+                       defaultValue:`${book!==null&&book.price||''}`,
+                      name: "price",
+                      ...register("price"),
+                    }}
+                    placeholder="Enter Price"
+                    error={Boolean(errors.price)}
+                    onChange={handleChange}
+                    fullWidth
+                  />
+                  {errors.price && (
+                    <p className="text-error">{errors.price?.message}</p>
+                  )}
+                </Grid>
               </Grid>
               <Grid item>
                 <InputCustom
-                  label="Announcement"
+                  label="description"
                   size="small"
-                  placeholder="Enter Announcement"
-                  error={Boolean(errors.content)}
+                  inputProps={{
+                     defaultValue:`${book!==null&&book.description||''}`,
+                    name: "description",
+                    ...register("description"),
+                  }}
+                  placeholder="Enter Description"
+                  error={Boolean(errors.description)}
                   onChange={handleChange}
                   multiline
-                  rows={5}
+                  minRows={5}
                   fullWidth
                 />
+                {errors.description && (
+                  <p className="text-error">{errors.description?.message}</p>
+                )}
               </Grid>
-              <Grid item>
-                <Button
-                  type="submit"
-                  className={`${classes.editBtn} _btn _btn-primary w-100`}
-                  disabled={submitting}
-                  endIcon={
-                    submitting ? <LoopIcon className="ani-spin" /> : <SaveIcon />
-                  }
-                  variant="contained"
-                >
-                 Save
-                </Button>
+              <Grid item container direction='row-reverse'>
+                <Grid item xs={4}>
+                  <Button
+                    type="submit"
 
+                    className={`${classes.editBtn} _btn _btn-primary w-100`}
+                    disabled={submitting}
+                    endIcon={
+                      submitting ? <LoopIcon className="ani-spin" /> : <SaveIcon />
+                    }
+                    variant="contained"
+                  >
+                    Save
+                  </Button>
+                </Grid>
               </Grid>
             </Grid>
-          </form>
+          </form>}
         </div>
       </div>
     </>
