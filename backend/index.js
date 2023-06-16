2369660c960c46c4e1fc9279f0e30a90e1d6d36c
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-16 22:14:59 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors());
-app.use(fileUpload({ useTempFiles: true }));
+app.use(fileUpload({ useTempFiles: true,createParentPath: true  }));
 
 const BASE_URL = '/api';
 app.use(`${BASE_URL}/user`, require('./routes/userRouter'));
@@ -54,71 +54,7 @@ mongoose.connect(URI, {
   console.log("Connected to MongoDB");
 });
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Alba123.",
-  database: "sakila",
-});
-
-app.get("/", (req, res) => {
-  res.json("hello");
-});
-
-app.get("/books", (req, res) => {
-  const q = "SELECT * FROM books";
-  db.query(q, (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.json(err);
-    }
-    return res.json(data);
-  });
-});
-
-app.post("/books", (req, res) => {
-  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
-
-  const values = [
-    req.body.title,
-    req.body.desc,
-    req.body.price,
-    req.body.cover,
-  ];
-
-  db.query(q, [values], (err, data) => {
-    if (err) return res.send(err);
-    return res.json(data);
-  });
-});
-
-app.delete("/books/:id", (req, res) => {
-  const bookId = req.params.id;
-  const q = " DELETE FROM books WHERE id = ? ";
-
-  db.query(q, [bookId], (err, data) => {
-    if (err) return res.send(err);
-    return res.json(data);
-  });
-});
-
-app.put("/books/:id", (req, res) => {
-  const bookId = req.params.id;
-  const q =
-    "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
-
-  const values = [
-    req.body.title,
-    req.body.desc,
-    req.body.price,
-    req.body.cover,
-  ];
-
-  db.query(q, [...values, bookId], (err, data) => {
-    if (err) return res.send(err);
-    return res.json(data);
-  });
-});
+app.use('/public/books', express.static(path.join(__dirname, 'uploads/books')));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("public"));
@@ -138,4 +74,4 @@ app.listen(PORT, () => {
 
 app.listen(8800, () => {
   console.log("Connected to MySQL");
-});  
+});
