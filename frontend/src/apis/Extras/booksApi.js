c5119447ddd0ed9de2a26305f42339ff2da063e0
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-17 14:29:55 +0200

    Update booksApi.js

--- frontend/src/apis/Extras/booksApi.js
+++ frontend/src/apis/Extras/booksApi.js
@@ -1,17 +1,23 @@
 import axios from "axios";
-const URL = `/api/book`;
+const URL = `/api/books`;
 const bookApi ={
     getbook:()=>{
         return axios.get(`${URL}/get-all-book`)
     },
-    updatebook:(id,title,authour,thumbnail)=>{
-        return axios.put(`${URL}/put-book/${id}`,{title,authour,thumbnail})
+    getbookbyid:(id)=>{
+        return axios.get(`${URL}/get-book-by-id/${id}`)
+    },
+    updatebook:(id,data)=>{
+        return axios.put(`${URL}/put-book/${id}`,data)
     },
     deletebook:(id)=>{
         return axios.delete(`${URL}/delete-book/${id}`)
     },
-    createbook:(title,authour,thumbnail)=>{
-        return axios.get(`${URL}/post-book`,{title,authour,thumbnail})
+    createbook:(data)=>{
+        return axios.post(`${URL}/post-book`,data)
+    },
+    updatecover:(id,data)=>{
+         return axios.put(`${URL}/put-book-cover/${id}`,data)
     }
 }
-export default bookApi
+export default bookApi  
