11664cad9e3ef5de8efbe1bf754d30f29b3d547c
AgonHH <35099216+AgonHH@users.noreply.github.com>
2023-06-17 18:14:52 +0200

    Update index.js

--- frontend/src/helper/index.js
+++ frontend/src/helper/index.js
@@ -20,7 +20,7 @@ export const equalArray = (a, b) => {
     Array.isArray(a) &&
     Array.isArray(b) &&
     a.length === b.length &&
-    a.every((val, index) => val === b[index])
+    a.every((val, index) => val === b[index]) 
   );
 };
 
