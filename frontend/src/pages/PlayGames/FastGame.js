8a1201944463fe4bcb0639306088be72070abc17
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-17 13:59:41 +0200

    Update FastGame.js

--- frontend/src/pages/PlayGames/FastGame.js
+++ frontend/src/pages/PlayGames/FastGame.js
@@ -2,8 +2,7 @@ import FastGameData from 'components/PlayGames/FastGame/data';
 import useCloseNavigation from 'hooks/useCloseNavigation';
 import useTitle from 'hooks/useTitle';
 import React from 'react';
-
-
+   
 function FastGamePage() { 
   useTitle('Game hands faster than brain');
   useCloseNavigation();
