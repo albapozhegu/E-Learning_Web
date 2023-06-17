ba59d4a2c874ac95eb0a0bb323e8fd99dec9310f
albaaaaaa <93519312+albapozhegu@users.noreply.github.com>
2023-06-17 14:16:09 +0200

    Update grammarLevels.js

--- frontend/src/constants/grammarLevels.js
+++ frontend/src/constants/grammarLevels.js
@@ -2,7 +2,7 @@ import Level1 from 'assets/images/grammarLevels/Level1.jpg';
 import Level2 from 'assets/images/grammarLevels/Level2.jpg';
 import Level3 from 'assets/images/grammarLevels/Level3.jpg';
 import Level4 from 'assets/images/grammarLevels/Level4.jpg';
-
+   
 export const GRAMMAR_LEVEL = [
     { 
       value: '1',
